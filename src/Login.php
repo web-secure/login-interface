1433c7445f6a453ce805a19a244feba19e5e0a42
Nicholas <nenglish7721@gmail.com>
2018-12-25 16:24:55 -0500

    Update Login.php

--- src/Login.php
+++ src/Login.php
@@ -1,1 +1,27 @@
+<?php
+declare(strict_types=1);
+/**
+ * WebSecure - WebAuthInterface.
+ * 
+ * @author Nicholas English <https://github.com/iszorpal>.
+ * @link <https://github.com/web-secure/web-auth-interface>.
+ */
 
+namespace WebSecure\Auth\Interface;
+
+/**
+ * The Login interface.
+ */
+interface Login
+{
+
+    /**
+     * Initialize the login class.
+     *
+     * @param array $loginParams                   The login parameters.
+     * @param \WebSecure\PasswordHashing\Interface The password hasher class.
+     *
+     * @return void.
+     */
+    public function __construct(array $loginParams = [], \WebSecure\PasswordHashing\Interface $passwordHashing);
+}
